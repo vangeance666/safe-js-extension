b1cca1da7b3cadbb216e1248fe28110f0d4300c4
Vangeance666 <1902132@sit.singaporetech.edu.sg>
2022-04-06 16:36:36 +0800

    Update main.js

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,45 +1,57 @@
+const apiRunPath = "/api/v1_0/analysis/run/";
+
+function setSuccess(eleId, text) {
+	const el = document.getElementById(eleId);
+	el.classList.remove('text-success', 'text-danger');
+	el.classList.add('text-success');
+	el.innerHTML = text;
+}
+
+function setError(eleId, text) {
+	const el = document.getElementById(eleId);	
+	el.classList.remove('text-success', 'text-danger');
+	el.classList.add('text-danger');
+	el.innerHTML = text;
+}
+
 function open(url, active = true) {
     chrome.tabs.create({ url, active })
 }
 
-function setApiUrlStatus() {
-	console.log("setApiUrlStatus");
-	// GET Settings url then send GET request to that url
-	function setStatusText(item) {
+function ApiHelper(url, method = 'POST', data = {}) {
+	const dict = method === 'POST' ? {
+		method: method,
+		headers : {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	} : { method: method }
 
-		if (!item || !item.apiDetails || !item.apiDetails.url) {
-			console.log("!item || !item.apiDetails || !item.apiDetails.url")
-			return	
-		} 
+	console.log("dict: ", dict);
 
-		function reqListener () {
-	  		if (this.status == 200 && this.responseText) {
-	  			let data = JSON.parse(this.responseText)
-	  			document.querySelector("#server-status-reply").innerHTML = data['status'];
-	  		}
-		}
 
-		let settingsUrl = item.apiDetails.url
-		let heartBeatUrl = new URL("/heartbeat/", settingsUrl)
-		let oReq = new XMLHttpRequest();
-		oReq.addEventListener("load", reqListener);
-		oReq.open("GET", heartBeatUrl);
-		oReq.send();
-	}
-
-	function onError(e) {
-		console.log(`Error: ${e}`);
-	}
-
-	let getting = browser.storage.local.get("apiDetails");
-	getting.then(setStatusText, onError);
+    return fetch(url, dict)
+    .then(res => res.json())
+    .then((result) => {
+        console.log(result);
+        return result;
+    }, (error) => {
+        console.log(`ApiHelper Error ${error}`);
+    })
 }
 
-function submitUrl() {
-	// Get textbox value
-	// 
-	// Check if API UP,
-	// POST to api URL,
+
+function setApiUrlStatus() {
+	browser.storage.local.get("apiDetails")
+	.then((item) => {
+		if (!item || !item.apiDetails || !item.apiDetails.url) return;
+		
+		ApiHelper(new URL("/heartbeat/", item.apiDetails.url), "GET")
+		.then(response => {		
+			document.querySelector("#server-status-reply").innerHTML = response.status || "Down"
+		})
+
+	}, (e) => { console.log(`Error ${e}`)});
 }
 
 
@@ -53,50 +65,63 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	setApiUrlStatus();
 
-
-	// Set default, if On will change to Healthy
 	document.querySelector("#server-status-reply").innerHTML = "Down";	
 
-	// let getting = browser.storage.local.get("apiDetails");
 
-
-	settingsBtn.addEventListener("click", function(e) {
+	settingsBtn.addEventListener("click", (e) => {
 		browser.runtime.openOptionsPage();		
 		console.log("settings click")
 	});
 
 
-	submitUrlBtn.addEventListener("click", function(e) {
+	submitUrlBtn.addEventListener("click", (e) => {
 		e.preventDefault();
 
-		let urlToAnalyze = document.getElementById("analyze-url-input").value
+		const inputUrl = document.getElementById("analyze-url-input").value
 
-		console.log("urlToAnalyze: ", urlToAnalyze);
+		if (!inputUrl) return
+		console.log("inputUrl: ", inputUrl);
 
+		browser.storage.local.get("apiDetails")
+		.then((item) => {
+			if (!item || !item.apiDetails || !item.apiDetails.url) return;
+			
+			const apiUrl = new URL(apiRunPath, item.apiDetails.url)
+
+			console.log("apiUrl: ", apiUrl);
+
+			ApiHelper(apiUrl, "POST", {
+			  "url": inputUrl,
+			  "mode": "single"
+			})
+			.then(response => {
+				console.log("submitUrlBtn response: ", response)
+
+				if (!response || !response.status)
+					setError("feedback-label", "Failed to submit")
+
+				setSuccess("feedback-label", response.status === "ok" 
+				? `Assigned Page: ${response.page_id}` : `Assigned Page: Nothing`)
+
+			})
+
+		}, (e) => { console.log(`browser.storage.local rror ${e}`)});
 	});
 
-	goResultsBtn.addEventListener("click", function(e) {
+	goResultsBtn.addEventListener("click", (e) => {
+		e.preventDefault();
 
-		function redirectToSettings(item) {
-			console.log("item: ", item);
+		browser.storage.local.get("apiDetails")
+		.then((item) => {
 			if (!item || !item.apiDetails || !item.apiDetails.url) {
 				console.log("!item || !item.apiDetails || !item.apiDetails.url")
 				return	
 			}
 			console.log("Before open")
 			open(item.apiDetails.url)
-		}
-
-		function onError(e) {
+		}, (e) => {
 			console.log(`Error: ${e}`);
-		}
-
-		e.preventDefault();
-		let getting = browser.storage.local.get("apiDetails");
-		console.log("getting: ", getting);
-
-
-		getting.then(redirectToSettings, onError);
+		});
 
 		// window.open("https://www.w3schools.com");
 
